2b50d3ce4cc1867ef88c983a6a6143209ea4a404
NastiaShulgina <75643824+NastiaShulgina@users.noreply.github.com>
2023-05-10 18:57:19 +0300

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
+
 const morgan = require('morgan')
 const morganBody = require('morgan-body')
-
-const app = express()
 
 morganBody(app)
 
 morgan.token('postData', (req) => req.method === 'POST' && JSON.stringify(req.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status - :response-time ms :postData'))
-
 
 let notes = [
     {
